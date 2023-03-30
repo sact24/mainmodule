8511a4a750cfc80f2ddc9a969b6616a1e27bf628
sact24 <sact24@gmail.com>
2023-03-30 23:31:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,6 @@ resource   "azurerm_virtual_network"   "vnet"   {
    name   =   var.subnet_name 
    resource_group_name   =    azurerm_resource_group.rg.name 
    virtual_network_name   =   azurerm_virtual_network.vnet.name 
-   address_prefix   =   "10.0.1.0/24" 
+   address_prefix   =   ["10.0.1.0/24"] 
  }
 
