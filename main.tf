8e2bc32d24a64ce38fd52e00b33f09126ed80c56
sact24 <sact24@gmail.com>
2023-04-02 21:11:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,21 +5,9 @@ provider "azurerm" {
 
 
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_name
+  name     = "jokamoka"
   location = "centralindia"
 }
 
-resource   "azurerm_virtual_network"   "vnet"   { 
-   name   =   "devvnet" 
-   address_space   =   [ "10.0.0.0/16" ] 
-   location   =   "centralindia" 
-   resource_group_name   =   azurerm_resource_group.rg.name 
- } 
 
- resource   "azurerm_subnet"   "subnet"   { 
-   name   =   var.subnet_name 
-   resource_group_name   =    azurerm_resource_group.rg.name 
-   virtual_network_name   =   azurerm_virtual_network.vnet.name 
-   address_prefixes   =   ["10.0.1.0/24"] 
- }
 
